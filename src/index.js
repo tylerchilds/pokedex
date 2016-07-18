4999667216cf7dbc5fc3b4d456799d0e800d7120
Tyler Childs <tylerwchilds@gmail.com>
2016-07-17 21:10:06 -0700

    removed awkward state query string

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history'
 
 import routes from './routes';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+
 ReactDOM.render(
-    <Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
-      {routes}
-    </Router>,
-    document.getElementById('app')
+  <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
+    {routes}
+  </Router>,
+  document.getElementById('app')
 );
